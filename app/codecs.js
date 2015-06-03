ba0a7279c5b83594358b68fb53c4e05c8421ab59
pszymon <pomykacz.sz@gmail.com>
2015-06-03 11:48:53 +0200

    Update codecs.js

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,34 @@
+/* exported codecA,codecB,codecC,codecD */
 function codecA(a, b)
 {
+    'use strict';
+    /*jshint bitwise: false*/
     if (a & b) {
-        return a + b;
+    return a + b;
     } else {
-        return a;
+    return a;
     }
 }
+
 function codecB(a, b)
 {
-    if (a & b) {
-        return a + b;
+    'use strict';
+    if (a && b) {
+    return a + b;
     } else {
-        return a + ',' + b;
+    return a + ',' + b;
     }
 }
+
 function codecC(a, b)
 {
+    'use strict';
+    /*jshint eqeqeq: false*/
     return a == b;
 }
+
 function codecD(a, b)
 {
-    return a == b;
+    'use strict';
+    return a === b;
 }
