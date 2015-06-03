e0ef8213f002cc34e9a03627e6a0b0cfe039b7d2
ppodraza <podraza.piotr@gmail.com>
2015-06-03 11:43:36 +0200

    Update codecs.js

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,35 @@
+/* exported codecA,codecB,codecC,codecD */
 function codecA(a, b)
 {
+    'use strict';
+    /*jshint bitwise: false*/
     if (a & b) {
         return a + b;
     } else {
         return a;
     }
 }
+
 function codecB(a, b)
 {
-    if (a & b) {
+    'use strict';
+    if (a && b) {
         return a + b;
     } else {
         return a + ',' + b;
     }
 }
+
 function codecC(a, b)
 {
+    'use strict';
+    /*jshint eqeqeq: false*/
     return a == b;
 }
+
 function codecD(a, b)
 {
-    return a == b;
+    'use strict';
+    return a === b;
 }
+
